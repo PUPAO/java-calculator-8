563dd86eab9db2fb8039a7164f57dfa7bddbda3a
PUPAO <tkdldjstm0127@gmail.com>
2025-10-16 17:51:52 +0900

    feat(setup): 구분자 2종류를 기준으로 분리된 숫자들의 총 합.
    기능 요구 사항 1번째
    * 문자열 내에서 쉼표, 클론으로 구분된 숫자들의 합을 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("덧셈할 문자열을 입력해 주세요.\n");
+        String[] basicStr = Console.readLine().split("[,:]");
+
+        int sum = 0;
+        for (String s : basicStr) {
+            sum += Integer.parseInt(s);
+        }
+
+        System.out.println("결과 : " + sum);
     }
 }
