6882b7d766d6d2b0ed99ac8eb79364379ce0a98d
PUPAO <tkdldjstm0127@gmail.com>
2025-10-19 14:31:25 +0900

    feat(parser): //와 \n 사이에 문자를 입력하여, 구분자를 추가한다.
    기능 요구 사항 2번째
    * //와 \n사이에 문자를 적어서 구분자로 등록하여, 계산기에서 ,: 외에 다양한 문자로 숫자 사이를 구분할 수 있다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,15 +3,36 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    static StringBuilder dividedWords = new StringBuilder(",|:|//|\\\\n");
+
     public static void main(String[] args) {
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
-        String[] basicStr = Console.readLine().split("[,:]");
+        String basicStr = Console.readLine();
 
-        int sum = 0;
-        for (String s : basicStr) {
-            sum += Integer.parseInt(s);
+        // 구분자를 등록
+        int lastLocation=-1;
+        while(true) {
+            int startLocation = basicStr.indexOf("//",lastLocation);
+            int endLocation = basicStr.indexOf("\\n", startLocation);
+            if (startLocation == -1) {
+                break;
+            }
+
+            String dividedWord = basicStr.substring(startLocation+2, endLocation);
+            dividedWords.append("|").append(dividedWord);
+            lastLocation = endLocation;
+        }
+
+        // 숫자와 구분자로 계산기 실행
+        String[] word = basicStr.split(String.valueOf(dividedWords));
+        int sum=0;
+        for(String s : word) {
+            if(s.isEmpty())
+                continue;
+            sum+=Integer.parseInt(s);
         }
 
         System.out.println("결과 : " + sum);
+        Console.close();
     }
 }
