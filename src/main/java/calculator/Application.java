b9270e64c01f7ec824342b33fd7ffa6a52232b57
PUPAO <tkdldjstm0127@gmail.com>
2025-10-19 16:41:23 +0900

    feat(refactoring): Java Style Guide을 적용함.
    * 애매한 변수명을 적합한 변수명을 수정함.
    * 필요성을 느끼지 못해서 메서드로 추출하지 않음.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,42 +3,43 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    static StringBuilder dividedWords = new StringBuilder(",|:|//|\\\\n");
+    static StringBuilder separatorList = new StringBuilder(",|:|//|\\\\n");
 
     public static void main(String[] args) {
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
-        String basicStr = Console.readLine();
+        String inputStr = Console.readLine();
 
         // 구분자를 등록
-        int lastLocation=-1;
-        while(true) {
-            int startLocation = basicStr.indexOf("//",lastLocation);
-            int endLocation = basicStr.indexOf("\\n", startLocation);
+        int lastLocation = -1;
+        while (true) {
+            int startLocation = inputStr.indexOf("//", lastLocation);
+            int endLocation = inputStr.indexOf("\\n", startLocation);
             if (startLocation == -1) {
                 break;
             }
 
-            String dividedWord = basicStr.substring(startLocation+2, endLocation);
-            dividedWords.append("|").append(dividedWord);
+            String separator = inputStr.substring(startLocation + 2, endLocation);
+            separatorList.append("|").append(separator);
             lastLocation = endLocation;
         }
 
         // 숫자와 구분자로 계산기 실행
-        String[] word = basicStr.split(String.valueOf(dividedWords));
-        int sum=0;
-        for(String s : word) {
-            if(s.isEmpty())
+        String[] elementList = inputStr.split(String.valueOf(separatorList));
+        int sum = 0;
+        for (String element : elementList) {
+            if (element.isEmpty()) {
                 continue;
+            }
 
             int number;
             // 구분자가 아닌 문자 여부 판별
             try {
-                number = Integer.parseInt(s);
-            } catch(Exception e){
+                number = Integer.parseInt(element);
+            } catch (Exception e) {
                 throw new IllegalArgumentException();
             }
             // 숫자 사이에 0 혹은 음수 판별
-            if(number <= 0 ){
+            if (number <= 0) {
                 throw new IllegalArgumentException();
             }
             sum += number;
